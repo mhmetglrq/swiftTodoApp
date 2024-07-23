deb6f6463b2ee2659acc35175230d75477cea3cb
Mehmet Guler <mhmetglr.q@gmail.com>
2024-07-23 15:04:58 +0300

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Mehmet Güler on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
