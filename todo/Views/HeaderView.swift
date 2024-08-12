294bf5a7d5fccbf68bd097487cd6d23bec253342
Mehmet Guler <mhmetglr.q@gmail.com>
2024-08-12 17:47:19 +0300

    Login View Updated!

---
+++ todo/Views/HeaderView.swift
@@ -0,0 +1,37 @@
+//
+//  HeaderView.swift
+//  todo
+//
+//  Created by Mehmet Güler on 12.08.2024.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 0).foregroundColor(Color.pink)
+                .rotationEffect(Angle(degrees: 15))
+                
+            VStack{
+                Text("To Do List")
+                    .font(.system(size: 50))
+                    .foregroundColor(Color.white)
+                    .bold()
+                Text("Get things done")
+                    .font(.system(size: 30))
+                    .foregroundColor(Color.white)
+            }.padding(
+                .top,30
+            )
+        }
+        .frame(
+            width: UIScreen.main.bounds.width*3  ,height: 300)
+        .offset(y:-100)
+        
+    }
+}
+
+#Preview {
+    HeaderView()
+}
--- todo/Views/LoginView.swift
+++ todo/Views/LoginView.swift
@@ -7,9 +7,47 @@
 
 import SwiftUI
 
+
+
 struct LoginView: View {
+    @State var email=""
+    @State var password=""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                //Header
+                HeaderView()
+                // Login Form
+                Form{
+                    TextField("Email Address",text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    SecureField("Password",text: $password
+                    )
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button{
+                        //Attemp Login
+                    } label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color.blue)
+                        Text("Log In")
+                                .foregroundColor(Color.white)
+                                .bold()
+                        }
+                    }
+                }
+                Spacer()
+                // Create Account
+                VStack{
+                    Text("New Around Here?")
+                    
+                    NavigationLink("Create An Account",destination: RegisterView())
+                    
+                }
+                .padding(.bottom,50)
+            }
+        }
     }
 }
 
